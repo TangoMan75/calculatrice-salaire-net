4712c933bfb4d1bd151cd46fef8c998477fa3d42
Matthias Morin <mat@tangoman.io>
2021-03-06 19:40:20 +0100

    2021-03-06 19:40:20

---
+++ precache-manifest.6b2add22d2775768d3062eb330785369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b40b01bc4aa90c82dd99",
+    "url": "/calculatrice-salaire-net/css/app.beaebedc.css"
+  },
+  {
+    "revision": "b40b01bc4aa90c82dd99",
+    "url": "/calculatrice-salaire-net/js/app.754d868e.js"
+  },
+  {
+    "revision": "a5e3e867e5fca8a26ed7",
+    "url": "/calculatrice-salaire-net/js/chunk-vendors.797f9953.js"
+  },
+  {
+    "revision": "25766ad54e57f6448055b4ae7582a52c",
+    "url": "/calculatrice-salaire-net/img/logo.25766ad5.png"
+  },
+  {
+    "revision": "e55f465b1ddb6eb0b16ae8827acd763b",
+    "url": "/calculatrice-salaire-net/index.html"
+  },
+  {
+    "revision": "a15c2ac3234aa8f6064ef9c1f7383c37",
+    "url": "/calculatrice-salaire-net/css/bootstrap.min.css"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/calculatrice-salaire-net/robots.txt"
+  }
+];
