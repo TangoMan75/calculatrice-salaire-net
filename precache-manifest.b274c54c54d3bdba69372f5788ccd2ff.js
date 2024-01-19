65e00955a74fd4aedf4b621038ab86e990bba26a
Matthias Morin <mat@tangoman.io>
2024-01-19 18:26:09 +0100

    2024-01-19 18:26:09

---
+++ precache-manifest.b274c54c54d3bdba69372f5788ccd2ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bd522a856c053cd1607",
+    "url": "/calculatrice-salaire-net/css/app.fb1ab125.css"
+  },
+  {
+    "revision": "0bd522a856c053cd1607",
+    "url": "/calculatrice-salaire-net/js/app.2a4a70f2.js"
+  },
+  {
+    "revision": "3f6df9315ac9b1c07d0a",
+    "url": "/calculatrice-salaire-net/js/chunk-vendors.0e084c8d.js"
+  },
+  {
+    "revision": "25766ad54e57f6448055b4ae7582a52c",
+    "url": "/calculatrice-salaire-net/img/logo.25766ad5.png"
+  },
+  {
+    "revision": "ac91dad6375d0fe4c73f3bf9fe791a30",
+    "url": "/calculatrice-salaire-net/index.html"
+  },
+  {
+    "revision": "a15c2ac3234aa8f6064ef9c1f7383c37",
+    "url": "/calculatrice-salaire-net/css/bootstrap.min.css"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/calculatrice-salaire-net/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/calculatrice-salaire-net/precache-manifest.b274c54c54d3bdba69372f5788ccd2ff.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "calculatrice-salaire-net"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
